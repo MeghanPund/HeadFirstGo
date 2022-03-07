60c0cdf867e474cbac37d4d713664cd5dfedf54b
Meghan Pund <meghan.pund@gmail.com>
2022-03-07 17:47:56 -0500

    vars

--- functionReturnValues.go
+++ functionReturnValues.go
@@ -10,11 +10,13 @@ import (
 func main() {
 	var half, marathon float64
 	half, marathon = 13.1, 26.2
+	hero := "Deena Kastor"
+	hero2 := "paula radcliffe"
 
 	fmt.Println(math.Floor(half))
-	fmt.Println(strings.Title("mosquito bites"))
+	fmt.Println(strings.Title(hero2))
 	fmt.Println("This is a string\nand there is a newline\nand another here!")
-	fmt.Println("Tabs\tall\tover\tthe\tplace!!!")
+	fmt.Println("Tabs\tall\tover\tthe\tplace!!!\t", hero)
 	fmt.Println(reflect.TypeOf(marathon))
 	fmt.Println(reflect.TypeOf("stringggg"))
 	fmt.Println(reflect.TypeOf(3.1415))
