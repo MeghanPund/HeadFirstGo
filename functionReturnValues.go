61a7c07e20558c03d5e6e96580cab9acb44793e6
Meghan Pund <meghan.pund@gmail.com>
2022-03-06 19:21:39 -0500

    added calls to reflect's TypeOf func

--- functionReturnValues.go
+++ functionReturnValues.go
@@ -3,10 +3,17 @@ package main
 import (
 	"fmt"
 	"math"
+	"reflect"
 	"strings"
 )
 
 func main() {
 	fmt.Println(math.Floor(2.75))
 	fmt.Println(strings.Title("mosquito bites"))
+	fmt.Println("This is a string\nand there is a newline\nand another here!")
+	fmt.Println("Tabs\tall\tover\tthe\tplace!!!")
+	fmt.Println(reflect.TypeOf(42))
+	fmt.Println(reflect.TypeOf("stringggg"))
+	fmt.Println(reflect.TypeOf(3.1415))
+	fmt.Println(reflect.TypeOf(true))
 }
