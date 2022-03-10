24f162607c1fd78cfa7532e4e04cbf858bc2108a
Meghan Pund <meghan.pund@gmail.com>
2022-03-10 07:55:52 -0500

    added 'miles'

--- functionReturnValues.go
+++ functionReturnValues.go
@@ -13,11 +13,11 @@ func main() {
 	hero2 := "paula radcliffe"
 	pizza := []int{1, 2, 3}
 
-	fmt.Println(math.Floor(half))
+	fmt.Println(math.Floor(half), "miles")
 	fmt.Println(strings.Title(hero2))
 	fmt.Println("This is a string\nand there is a newline\nand another here!")
 	fmt.Println("Tabs\tall\tover\tthe\tplace!!!\t", hero)
-	fmt.Println(reflect.TypeOf(marathon))
+	fmt.Println(reflect.TypeOf(marathon), "miles")
 	fmt.Println(reflect.TypeOf("stringggg"))
 	fmt.Println(reflect.TypeOf(3.1415))
 	fmt.Println(reflect.TypeOf(true))
