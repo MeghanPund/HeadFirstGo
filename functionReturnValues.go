b3ed96d5e11b16eff43ae870d269035452be039e
Meghan Pund <meghan.pund@gmail.com>
2022-03-28 17:14:05 -0400

    commented out main function so main function from other file can reign supreme

--- functionReturnValues.go
+++ functionReturnValues.go
@@ -1,25 +1,18 @@
 package main
 
-import (
-	"fmt"
-	"math"
-	"reflect"
-	"strings"
-)
+// func main() {
+// 	half, marathon := 13.1, 26.2
+// 	hero := "Deena Kastor"
+// 	hero2 := "paula radcliffe"
+// 	pizza := []int{1, 2, 3}
 
-func main() {
-	half, marathon := 13.1, 26.2
-	hero := "Deena Kastor"
-	hero2 := "paula radcliffe"
-	pizza := []int{1, 2, 3}
-
-	fmt.Println(math.Floor(half), "miles")
-	fmt.Println(strings.Title(hero2))
-	fmt.Println("This is a string\nand there is a newline\nand another here!")
-	fmt.Println("Tabs\tall\tover\tthe\tplace!!!\t", hero)
-	fmt.Println(reflect.TypeOf(marathon))
-	fmt.Println(reflect.TypeOf("stringggg"))
-	fmt.Println(reflect.TypeOf(3.1415))
-	fmt.Println(reflect.TypeOf(true))
-	fmt.Println(pizza[0])
-}
+// 	fmt.Println(math.Floor(half), "miles")
+// 	fmt.Println(strings.Title(hero2))
+// 	fmt.Println("This is a string\nand there is a newline\nand another here!")
+// 	fmt.Println("Tabs\tall\tover\tthe\tplace!!!\t", hero)
+// 	fmt.Println(reflect.TypeOf(marathon))
+// 	fmt.Println(reflect.TypeOf("stringggg"))
+// 	fmt.Println(reflect.TypeOf(3.1415))
+// 	fmt.Println(reflect.TypeOf(true))
+// 	fmt.Println(pizza[0])
+// }
